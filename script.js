2bd3d897b629b194a01318431063298830f0da4f
rathoraryann <arrathor1334@gmail.com>
2025-12-08 00:37:33 +0530

    project complete

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const track = document.querySelector('.slide-track');
-let speed = 40; // lower = faster
+const bookAppointMentBtn = document.querySelector(".small-btn");
+const focusElement = document.getElementById("name-input");
 
-track.style.animationDuration = speed + "s";
-
-
-
-document.querySelectorAll(".faq-item").forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("open");
-    });
+bookAppointMentBtn.addEventListener('click', () => {
+    focusElement.scrollIntoView({ behavior: "smooth", block: "center" });
+    focusElement.focus();
 });
